2df40724cc4e3f1cfaf54736349cbc502589204d
tahayvr <paprikataha@gmail.com>
2026-09-04 09:54:31 -0700

    Update logo files

--- src/lib/components/brand/Logo.svelte
+++ src/lib/components/brand/Logo.svelte
@@ -1,10 +1,12 @@
 <script>
-	import Logo from '$lib/assets/soo-logo.png';
-	import Year2026 from '$lib/components/brand/year/2026.svelte';
+	import LogoDark from '$lib/assets/logo/soo2026-logo-dark.png';
+	import LogoLight from '$lib/assets/logo/soo2026-logo-light.png';
 </script>
 
 <div class="flex-col justify-center gap-2">
-	<img src={Logo} alt="State of Omarchy 2026" class="w-lg opacity-50" />
-	<Year2026 />
-	<h2 class="mt-2 text-center text-muted-foreground">The yearly survey about Omarchy Linux</h2>
+	<img src={LogoDark} alt="State of Omarchy 2026" class="hidden w-lg dark:block" />
+	<img src={LogoLight} alt="State of Omarchy 2026" class="w-lg dark:hidden" />
+	<h2 class="mt-2 text-center font-mono text-muted-foreground">
+		The yearly survey about <a href="https://omarchy.org/" target="_blank">Omarchy Linux</a>
+	</h2>
 </div>
